8f5c03ba93d54920132914146c23dfedfa91cb08
pahill <pamela.a.hill@gmail.com>
2024-06-25 14:24:13 +0200

    Update to v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform").version("1.9.20-RC")
-    kotlin("native.cocoapods").version("1.9.20-RC")
+    kotlin("multiplatform").version("2.0.0")
+    kotlin("native.cocoapods").version("2.0.0")
 }
 
 group = "org.example"
